0e3beca43179b61267723ec6f7a4e09a9edeb06e
Vlad-R <Vlad-R@users.noreply.github.com>
2015-03-01 00:16:01 +0200

    Added project base

---
+++ PokerSimulator/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  PokerSimulator
+//
+//  Created by Vlad-R on 01/03/15.
+//  Copyright (c) 2015 Vlad-R. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
